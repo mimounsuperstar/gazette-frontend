71a98baae7d8b47128c900bb0ae2aeb49127c59f
mimounsuperstar <119753995+mimounamar@users.noreply.github.com>
2023-02-09 20:15:52 +0100

    Updated errors

--- src/cupideon/helpers/questions.js
+++ src/cupideon/helpers/questions.js
@@ -1,15 +1,15 @@
 export const questionsData = [
     {
-        name:"Ton match doit ...",
-        questions: [["A", "t’être opposé."],["B", "te compléter."]]
+        name:"Ta plus grande qualité est...",
+        questions: [["A", "ton intelligence."],["B" , "ton humour."],["C" , "ta beauté."],["D" , "ton empathie."], ["E" , "ta joie de vivre."], ["F" , "ta fidélité."], ["G" , "ta créativité."], ["H" , "ta patience."], ["I" , "ton honnêteté."]]
     },
     {
-        name:"Ta plus grande qualité est...",
-        questions: [["A", "ton intelligence."],["B" , "ton humour."],["C" , "ta beauté."],["D" , "ton empathie."], ["E" , "ta joie de vivre."], ["f" , "ta fidélité."], ["G" , "ta créativité."], ["H" , "ta patience."], ["I" , "ton honnêteté."]]
+        name:"Tu recherches chez ton partenaire...",
+        questions: [["A", "de l'intelligence."],["B" , "de l'humour."],["C" , "de la beauté."],["D" , "de l'empathie."], ["E" , "de la joie de vivre."], ["F" , "de la fidélité."], ["G" , "de la créativité."], ["H" , "de la patience."], ["I" , "de l'honnêteté."]]
     },
     {
         name:"Quel est ton date parfait?",
-        questions: [["A", "Netflix & Chill"],["B" , "Pique-nique à la plage"],["C" , "Cinéma"],["D" , "Visite d'un musée"], ["E" , "Après-midi cuisine"], ["f" , "Plan foireux de dernière minute"], ["G" , "Grosse soirée"]]
+        questions: [["A", "Netflix & Chill"],["B" , "Pique-nique à la plage"],["C" , "Cinéma"],["D" , "Visite d'un musée"], ["E" , "Après-midi cuisine"], ["F" , "Plan foireux de dernière minute"], ["G" , "Grosse soirée"]]
     },
     {
         name:"Qui paie l’addition?",
@@ -24,15 +24,15 @@ export const questionsData = [
         questions: [["A", "gênant"],["B" , "inévitable, c’est la base"],["C" , "pas un problème, mais pas une nécessité."]]
     },
     {
-        name:"Avez vous peur de la mort?",
+        name:"As-tu peur de la mort?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Croyez-vous en l’existence des dinosaures?",
+        name:"Crois-tu en l’existence des dinosaures?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Êtes vous ouverts sur vos sentiments?",
+        name:"Es-tu ouvert sur vos sentiments?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
@@ -40,59 +40,63 @@ export const questionsData = [
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Ressentez-vous la nécessité de constamment rester en contact avec les gens?",
+        name:"Ressens-tu la nécessité de constamment rester en contact avec les gens?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"L’art est-il important pour vous?",
+        name:"L’art est-il important pour toi?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"La liberté de la presse est-elle, selon vous, importante?",
+        name:"La liberté de la presse est-elle, selon toi, importante?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Quelle est votre saison préférée?",
+        name:"Quelle est ta saison préférée?",
         questions: [["A", "AUTOMNE"],["B" , "HIVER"] ,["C" , "PRINTEMPS"],["C" , "ÉTÉ"]]
     },
     {
-        name:"Souriez-vous souvent aux gens que vous croisez?",
+        name:"Souris-tu souvent aux gens que tu croises?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Pensez-vous que les gens les plus riches doivent payer plus de taxes que les autres?",
+        name:"Penses-tu que les gens les plus riches doivent payer plus de taxes que les autres?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Les blagues racistes vous dérangent-t-elles?",
+        name:"Les blagues racistes te dérangent-elles?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Est-ce important, pour vous, que votre partenaire prenne soin de son corps?",
+        name:"Est-ce important, pour toi, que ton partenaire prenne soin de son corps?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Prenez-vous soit de votre corps?",
+        name:"Prends-tu soin de votre corps?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Êtes-vous heureux de la vie que vous menez?",
+        name:"Es-tu heureux de la vie que tu mènes?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Aimez-vous les animaux?",
+        name:"Aimes-tu les animaux?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Vous préférez l’amour ou l’argent?",
+        name:"Aimes-tu les papouilles?",
+        questions: [["A", "OUI"],["B" , "NON"]]
+    },
+    {
+        name:"Tu préféres l’amour ou l’argent?",
         questions: [["A", "L'amour"],["B" , "L'argent"]]
     },
     {
-        name:"Selon vous, l’âme sœur existe-t-elle?",
+        name:"Selon toi, l’âme sœur existe-t-elle?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
     {
-        name:"Selon vous, l’amour est-il éternel?",
+        name:"Selon toi, l’amour est-il éternel?",
         questions: [["A", "OUI"],["B" , "NON"]]
     },
 ]
