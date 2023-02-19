a09e9af829ebc3633803f4d079d1bea2bb87ba82
mimounsuperstar <119753995+mimounamar@users.noreply.github.com>
2023-02-19 16:33:44 +0100

    First Edition

--- src/reader/pages/Homepage.js
+++ src/reader/pages/Homepage.js
@@ -9,7 +9,7 @@ import {
     HomeWrapper, TitleContainer
 } from "../styled/Homepage.styled";
 import LittThumbnail_Mars2023 from "../helpers/thumbnails/litt_amelie_nothomb_0323.jpg";
-import MusiqueThumbnail_Mars2023 from "../helpers/thumbnails/art_ivan_le_terrible_0323.jpg";
+import MusiqueThumbnail_Mars2023 from "../helpers/thumbnails/musique_laylow_0323.jpg";
 import PhiloThumbnail_Mars2023 from "../helpers/thumbnails/philo_machine_experience_0323.jpg";
 import ArtThumbnail_Mars2023 from "../helpers/thumbnails/art_ivan_le_terrible_0323.jpg";
 import ActuThumbnail_Mars2023 from "../helpers/thumbnails/actu_chine_vs_us_tensions_0323.jpg";
@@ -18,7 +18,7 @@ import AstroThumbnail_Mars2023 from "../helpers/thumbnails/astro_outil_connaissa
 import CineThumbnail_Mars2023 from "../helpers/thumbnails/cine_batman_hero_decennies_0323.jpeg";
 import PhiloBisThumbnail_Mars2023 from "../helpers/thumbnails/philo_reve_metaphysique_physiologie_0323.jpg";
 import MicroThumbnail_Mars2023 from "../helpers/thumbnails/micro_oeuvre_independante_artiste_0323.jpg";
-import SportThumbnail_Mars2023 from "../helpers/thumbnails/art_ivan_le_terrible_0323.jpg";
+import SportThumbnail_Mars2023 from "../helpers/thumbnails/sport_chan_0323.jpg";
 import ItvThumbnail_Mars2023 from "../helpers/thumbnails/itv_vauthier_0323.JPG";
 
 function redirect(id){
@@ -101,7 +101,7 @@ function Homepage() {
                     <HomeSection xAxis={"1/3"} yAxis={"16/18"} onClick={()=>redirect("videos/itv_mme_vauthier_0323")}>
                         <HomeImage src={ItvThumbnail_Mars2023}/>
                         <TitleContainer>
-                            <ArticleTitle>Interview: Mme. VAUTHIER répond à vos questions.</ArticleTitle>
+                            <ArticleTitle>ActuLéon: CVL, LéoClub, Interview.</ArticleTitle>
                         </TitleContainer>
                     </HomeSection>
                 </HomeGrid>
